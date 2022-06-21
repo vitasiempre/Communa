d1fe4269b28b6667fb40c250b36c4d9736344f43
vitasiempre <wavelolmu@gmail.com>
2022-06-21 12:47:32 +0300

    верстаю вход

---
+++ app/components/shared/Button/button_component.rb
@@ -0,0 +1,8 @@
+class ButtonComponent < ViewComponent::Base
+  def initialize(type,
+               size: :normal,
+               icon: { name: nil, size: nil, color: nil },
+               color: :default,
+               link: nil)
+   end
+end
